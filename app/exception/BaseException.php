bf97e8790f8acd85942ec13e718b5ebbc970af1a
hhjmk <78078552+skyhhjmk@users.noreply.github.com>
2025-12-11 16:16:34 +0800

    修复 PHP8.4 语法问题

--- app/exception/BaseException.php
+++ app/exception/BaseException.php
@@ -9,7 +9,7 @@ class BaseException extends Exception
 {
     protected $data = [];
 
-    public function __construct(string $message = '', int $code = 0, Throwable $previous = null, array $data = [])
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null, array $data = [])
     {
         parent::__construct($message, $code, $previous);
         $this->data = $data;
